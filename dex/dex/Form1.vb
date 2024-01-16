10df735e86a3c52805bd73ae96acc908dc593f21
Royalhyena <Royalhyena8@gmail.com>
2024-01-16 13:57:34 -0700

    added fields

--- dex/dex/Form1.vb
+++ dex/dex/Form1.vb
@@ -41,8 +41,21 @@ Public Class Form1
         If IO.File.Exists("Data.txt") Then
             Dim inFile As New IO.StreamReader("Data.txt")
             records(0) = inFile.ReadLine
-            records(1) = inFile.ReadLine
             inFile.Close()
+            showrecord(0)
+
+        End If
+    End Sub
+    Public Sub showrecord(index As Integer)
+        Dim fields() As String
+        fields = records(index).Split("|")
+        Field1.Text = fields(0)
+        Field2.Text = fields(1)
+        Field3.Text = fields(2)
+        Field4.Text = fields(3)
+        Field5.Text = fields(4)
+        If File.Exists(fields(5)) Then
+            PictureBox1.Load(fields(5))
         End If
     End Sub
 End Class
